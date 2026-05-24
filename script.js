bb277f991f3633fa7b8abbeebc2123c02f3997c9
Tanish Malhotra <tanish.me2@gmail.com>
2026-05-24 23:54:41 +0530

    grid created

---
+++ script.js
@@ -0,0 +1,21 @@
+const board=document.querySelector('.board');
+
+const blockHeight=80;
+const blockWidth=80;
+
+const  cols=Math.floor(board.clientWidth/blockWidth);
+const  rows=Math.floor(board.clientHeight/blockHeight);
+
+
+
+for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+        const block = document.createElement('div');
+        block.classList.add("block");
+        board.append(block);
+        
+        
+    }
+    
+}
+
