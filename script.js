3b2891b5fb2a1375eebc8ad67a689a2ca5ffff3a
Tanish Malhotra <tanish.me2@gmail.com>
2026-05-27 02:50:45 +0530

    add direction logic

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const blockWidth=50;
 const  cols=Math.floor(board.clientWidth/blockWidth);
 const  rows=Math.floor(board.clientHeight/blockHeight);
 
-const direction= "right";
+let direction= "down";
 
 const blocks=[];
-const snake=[{x:1 , y:3},{x:1 , y:4},{ x:1 , y:5}];
+const snake=[{x:1 , y:3}];
 
 for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
@@ -33,10 +33,47 @@ function render(){
 }
 
 setInterval(() => {
-    
 
+    let head=null;
+
+    if(direction == "right"){
+        head={ x:snake[0].x,y:(snake[0].y)+1};
+    }
+    else if(direction == "left"){
+         head={ x:snake[0].x,y:(snake[0].y)-1};
+    }
+    else if(direction == "up"){
+         head={ x:snake[0].x-1,y:(snake[0].y)};
+    }
+    else if(direction == "down"){
+         head={ x:snake[0].x+1,y:(snake[0].y)};
+    }
+
+
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.remove("fill") ;
+
+        
+    });
+    snake.unshift(head);
+    snake.pop();
 
 
     render();
-}, 300); // in settimeout and set interval time is given in ms
+}, 200); // in settimeout and set interval time is given in ms
 
+addEventListener("keydown",(event)=>{
+    console.log(event.key);
+    if(event.key=="ArrowUp"){
+        direction="up";
+    }
+    else if(event.key=="ArrowDown"){
+        direction="down";
+    }
+    else if(event.key=="ArrowRight"){
+        direction="right";
+    }
+    else if(event.key=="ArrowLeft"){
+        direction="left";
+    }
+})
